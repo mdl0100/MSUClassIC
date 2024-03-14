eae991738656806b16772f2951be3700eda39a62
Marcos Lopez <7751091+mdl0100@users.noreply.github.com>
2024-03-14 12:36:00 -0500

    Firebase API integration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth, onAuthStateChanged } from 'firebase/auth';
-
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,8 +17,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp)
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 onAuthStateChanged(auth, user => {
